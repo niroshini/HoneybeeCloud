ad3f203f5a61501098d68f440467b073556d9c4b
Samir Shrestha <shresthasam@deakin.edu.au>
2022-12-01 22:42:37 +1100

    Increased ping timeout to deal with synchronous large file uploads

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
-	pingTimeout: 30000,
+	pingTimeout: 600000,
 	pingInterval: 2500
 });
 const formidable = require('formidable');
